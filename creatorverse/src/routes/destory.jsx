cbeab9fb2e4bfb700040c2d3bc91a928a90407eb
Shashank H M <hmshashankhm686@gmail.com>
2024-08-25 11:10:19 +0530

    Modified Code

--- creatorverse/src/routes/destory.jsx
+++ creatorverse/src/routes/destory.jsx
@@ -2,7 +2,7 @@ import { redirect } from "react-router-dom";
 import { deleteContact } from "../contacts";
 
 export async function action({ params }) {
-  throw new Error("Oh dang!");
+  // throw new Error("Oh dang!");
   await deleteContact(params.contactId);
   return redirect("/");
 }
